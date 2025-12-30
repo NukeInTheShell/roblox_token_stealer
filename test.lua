e837c2a2a3e30bf0ff3d53ba7fb76bce50cffd83
VITIO <62482694+NukeInTheShell@users.noreply.github.com>
2025-12-30 19:06:01 +0100

    Update Discord webhook message content and method

--- test.lua
+++ test.lua
@@ -4,10 +4,17 @@ local HttpService = game:GetService("HttpService")
 -- URL de ton webhook Discord
 local webhook = "https://discord.com/api/webhooks/1455615155969851403/EgT6gsKtBbGhgJ1gMDBWpD00lc5pU1m4Slkda6IU6ZaMOa5elH7KdVj3IGdBvF6jyAt0"
 
--- Message à envoyer
 local data = {
-    ["content"] = "Message envoyé depuis un Script serveur!"
+    content = "Message envoyé depuis Delta 🚀"
 }
 
-local jsonData = HttpService:JSONEncode(data)
-HttpService:PostAsync(webhook, jsonData, Enum.HttpContentType.ApplicationJson)
+local json = game:GetService("HttpService"):JSONEncode(data)
+
+request({
+    Url = webhook,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = json
+})
