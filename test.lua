91cbca0b42a614db79e55b61f8c376619fc74aef
VITIO <62482694+NukeInTheShell@users.noreply.github.com>
2025-12-30 19:06:21 +0100

    Remove unused HttpService variable

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
--- Activer HttpService
-local HttpService = game:GetService("HttpService")
+
 
 -- URL de ton webhook Discord
 local webhook = "https://discord.com/api/webhooks/1455615155969851403/EgT6gsKtBbGhgJ1gMDBWpD00lc5pU1m4Slkda6IU6ZaMOa5elH7KdVj3IGdBvF6jyAt0"
